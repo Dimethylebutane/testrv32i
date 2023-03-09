4800c9b1012e6a08aff0bbbbba0df158bdda0634
Dimethylebutane <39008241+Dimethylebutane@users.noreply.github.com>
2023-03-09 21:03:20 +0100

    make it run

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
-#/bin/sh
-
+ls
 #import all sources
 ghdl -i --workdir=work *.vhdl
-
 #make project
 ghdl -m --workdir=work test_bench
-
 #run simulation
 ghdl -r --workdir=work test_bench --wave=wave.ghw
